af086ee4830a87e4dea8417e1d479a2e1803f99e
DevOps School <44023974+devops-school@users.noreply.github.com>
2023-02-07 07:32:14 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,22 @@
 # Define the class for the module
 class websphere_app_server {
 
+ # Install WebSphere Application Server
+file { '/tmp/WAS.zip':
+  ensure => present,
+  source => '/tmp/WAS.zip',
+}
+
+ # Extract WebSphere Application Server
+exec { 'extract_zip':
+  command => 'unzip /tmp/WAS.zip -d /tmp',
+  onlyif  => 'test -f /tmp/WAS.zip',
+}
   # Install WebSphere Application Server
   exec { 'Install WebSphere Application Server':
     command => '/opt/IBM/InstallationManager/eclipse/tools/imcl install com.ibm.websphere.NDTRIAL.v85_8.5.5000.20130514_1044 -repositories /tmp/WAS/repository.config -acceptLicense',
     cwd     => '/opt/IBM/InstallationManager/eclipse/tools',
   }
+  
 }
 
