ac37e3375c6fa20a74a729753c2ecb960b024f1f
DevOps School <44023974+devops-school@users.noreply.github.com>
2023-02-07 07:19:46 +0530

    Update removewas.pp

--- manifests/removewas.pp
+++ manifests/removewas.pp
@@ -1,2 +1,14 @@
 class websphere_app_server::removewas (
+
+  # Remove WebSphere Application Server
+  exec { 'Remove WebSphere Application Server':
+    command => '/opt/IBM/InstallationManager/eclipse/tools/imcl imcl uninstall com.ibm.websphere.NDTRIAL.v85_8.5.5000.20130514_1044',
+    cwd     => '/opt/IBM/InstallationManager/eclipse/tools',
+  }
+  # Remove WebSphere Application Server
+  exec { 'Remove WebSphere Application Server':
+    command => 'rm -rf /opt/IBM/WebSphere/',
+    cwd     => '/opt/IBM/InstallationManager/eclipse/tools',
+  }
+  
 )
